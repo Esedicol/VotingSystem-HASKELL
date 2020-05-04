e7af298e7972f22e87bf6c1779ada524e1510901
Emmanuel Sedicol <sedicolemmanuel@gmail.com>
2020-05-04 13:08:22 +0100

    Mode function into modules and able to get count of preferences

---
+++ TupleFunc.hs
@@ -0,0 +1,33 @@
+module TupleFunc where
+
+import Data.List
+import Data.List (sortBy)
+import Data.Function (on)
+
+sortTuplesAscending :: Ord b => [(a, b)] -> [(a, b)]
+sortTuplesAscending = sortBy (compare `on` snd)
+
+sortTuplesDescending :: Ord b => [(a, b)] -> [(a, b)]
+sortTuplesDescending = sortBy (flip compare `on` snd)
+
+removeBlankVotes :: (Eq b, Num b) => [(a, b)] -> [(a, b)]
+removeBlankVotes xs = [(fst x, snd x) | x <- xs, snd x /= 0]
+
+changeToZero :: Num p1 => p2 -> p1
+changeToZero x = 0 
+
+changeSndToZero :: Num b => [(a, p2)] -> [(a, b)]
+changeSndToZero [] = []
+changeSndToZero ((a1, a2) : xs) = (a1, changeToZero a2) : changeSndToZero xs
+
+-- [("D",1),("E",2),("C",3),("B",3),("A",4)] -> [("D",1),("E",2),("C",0),("B",0),("A",0)]
+checkDuplicatesInTuple :: (Eq b, Num b) => [(a, b)] -> [(a, b)]  
+checkDuplicatesInTuple ((a1, a2) : []) = [(a1, a2)]    
+checkDuplicatesInTuple ((a1, a2) : xs) | a2 == snd (head xs) = (a1, changeToZero a2) : changeSndToZero xs
+            | otherwise = ((a1, a2) : checkDuplicatesInTuple xs)
+
+-- [("D",1),("E",2),("C",3),("B",5),("A",4)] -> [("D",1),("E",2),("C",3),("B",0),("A",0)]
+checkPreferenceOrder :: (Eq b, Num b) => [(a, b)] -> [(a, b)]
+checkPreferenceOrder ((a1, a2) : []) = [(a1, a2)]
+checkPreferenceOrder ((a1, a2) : xs) | snd (head xs) /= (a2 + 1) = ((a1, a2) : changeSndToZero xs)
+            | otherwise = ((a1, a2) : checkPreferenceOrder xs)
